2ae8ebdfd1d1e1c8e6a02f70f0403e684f411f6d
Astro36 <astr36@naver.com>
2018-03-04 21:44:58 +0900

    Remove unused code and beautify the code

--- lib/word2vec.cc
+++ lib/word2vec.cc
@@ -639,84 +639,7 @@ int ArgPos(char *str, int argc, char **argv) {
   return -1;
 }
 
-int Default222(int argc, char **argv) {
-  int i;
-  if (argc == 1) {
-    printf("WORD VECTOR estimation toolkit v 0.1c\n\n");
-    printf("Options:\n");
-    printf("Parameters for training:\n");
-    printf("\t-train <file>\n");
-    printf("\t\tUse text data from <file> to train the model\n");
-    printf("\t-output <file>\n");
-    printf("\t\tUse <file> to save the resulting word vectors / word clusters\n");
-    printf("\t-size <int>\n");
-    printf("\t\tSet size of word vectors; default is 100\n");
-    printf("\t-window <int>\n");
-    printf("\t\tSet max skip length between words; default is 5\n");
-    printf("\t-sample <float>\n");
-    printf("\t\tSet threshold for occurrence of words. Those that appear with higher frequency in the training data\n");
-    printf("\t\twill be randomly down-sampled; default is 1e-3, useful range is (0, 1e-5)\n");
-    printf("\t-hs <int>\n");
-    printf("\t\tUse Hierarchical Softmax; default is 0 (not used)\n");
-    printf("\t-negative <int>\n");
-    printf("\t\tNumber of negative examples; default is 5, common values are 3 - 10 (0 = not used)\n");
-    printf("\t-threads <int>\n");
-    printf("\t\tUse <int> threads (default 12)\n");
-    printf("\t-iter <int>\n");
-    printf("\t\tRun more training iterations (default 5)\n");
-    printf("\t-min-count <int>\n");
-    printf("\t\tThis will discard words that appear less than <int> times; default is 5\n");
-    printf("\t-alpha <float>\n");
-    printf("\t\tSet the starting learning rate; default is 0.025 for skip-gram and 0.05 for CBOW\n");
-    printf("\t-classes <int>\n");
-    printf("\t\tOutput word classes rather than word vectors; default number of classes is 0 (vectors are written)\n");
-    printf("\t-debug <int>\n");
-    printf("\t\tSet the debug mode (default = 2 = more info during training)\n");
-    printf("\t-binary <int>\n");
-    printf("\t\tSave the resulting vectors in binary moded; default is 0 (off)\n");
-    printf("\t-save-vocab <file>\n");
-    printf("\t\tThe vocabulary will be saved to <file>\n");
-    printf("\t-read-vocab <file>\n");
-    printf("\t\tThe vocabulary will be read from <file>, not constructed from the training data\n");
-    printf("\t-cbow <int>\n");
-    printf("\t\tUse the continuous bag of words model; default is 1 (use 0 for skip-gram model)\n");
-    printf("\nExamples:\n");
-    printf("./word2vec -train data.txt -output vec.txt -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -binary 0 -cbow 1 -iter 3\n\n");
-    return 0;
-  }
-  output_file[0] = 0;
-  save_vocab_file[0] = 0;
-  read_vocab_file[0] = 0;
-  if ((i = ArgPos((char *)"-size", argc, argv)) > 0) layer1_size = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-train", argc, argv)) > 0) strcpy(train_file, argv[i + 1]);
-  if ((i = ArgPos((char *)"-save-vocab", argc, argv)) > 0) strcpy(save_vocab_file, argv[i + 1]);
-  if ((i = ArgPos((char *)"-read-vocab", argc, argv)) > 0) strcpy(read_vocab_file, argv[i + 1]);
-  if ((i = ArgPos((char *)"-debug", argc, argv)) > 0) debug_mode = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-binary", argc, argv)) > 0) binary = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-cbow", argc, argv)) > 0) cbow = atoi(argv[i + 1]);
-  if (cbow) alpha = 0.05;
-  if ((i = ArgPos((char *)"-alpha", argc, argv)) > 0) alpha = atof(argv[i + 1]);
-  if ((i = ArgPos((char *)"-output", argc, argv)) > 0) strcpy(output_file, argv[i + 1]);
-  if ((i = ArgPos((char *)"-window", argc, argv)) > 0) window = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-sample", argc, argv)) > 0) sample = atof(argv[i + 1]);
-  if ((i = ArgPos((char *)"-hs", argc, argv)) > 0) hs = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-negative", argc, argv)) > 0) negative = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-threads", argc, argv)) > 0) num_threads = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-iter", argc, argv)) > 0) iter = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-min-count", argc, argv)) > 0) min_count = atoi(argv[i + 1]);
-  if ((i = ArgPos((char *)"-classes", argc, argv)) > 0) classes = atoi(argv[i + 1]);
-  vocab = (struct vocab_word *)calloc(vocab_max_size, sizeof(struct vocab_word));
-  vocab_hash = (int *)calloc(vocab_hash_size, sizeof(int));
-  expTable = (real *)malloc((EXP_TABLE_SIZE + 1) * sizeof(real));
-  for (i = 0; i < EXP_TABLE_SIZE; i++) {
-    expTable[i] = exp((i / (real)EXP_TABLE_SIZE * 2 - 1) * MAX_EXP); // Precompute the exp() table
-    expTable[i] = expTable[i] / (expTable[i] + 1);                   // Precompute f(x) = x / (x + 1)
-  }
-  TrainModel();
-  return 0;
-}
-
-// input_path, output_path, size, alpha, window, min_count, sample, workers, hs, negative, iter, cbow
+// args: input_path, output_path, size, alpha, window, min_count, sample, workers, hs, negative, iter, cbow
 napi_value Default(napi_env env, napi_callback_info info) {
   napi_status status;
 
@@ -764,112 +687,121 @@ napi_value Default(napi_env env, napi_callback_info info) {
 
   // size
   if (argc >= 3) {
-  napi_valuetype valuetype_size;
-  status = napi_typeof(env, args[2], &valuetype_size);
-  assert(status == napi_ok);
-  if (valuetype_size == napi_number) {
-    double size;
-    status = napi_get_value_double(env, args[2], &size);
-    layer1_size = (long long) size;
+    napi_valuetype valuetype_size;
+    status = napi_typeof(env, args[2], &valuetype_size);
     assert(status == napi_ok);
-  }
+    if (valuetype_size == napi_number) {
+      double size;
+      status = napi_get_value_double(env, args[2], &size);
+      layer1_size = (long long) size;
+      assert(status == napi_ok);
+    }
   }
 
   // alpha
   if (argc >= 4) {
-  napi_valuetype valuetype_alpha;
-  status = napi_typeof(env, args[3], &valuetype_alpha);
-  assert(status == napi_ok);
-  if (valuetype_alpha == napi_number) {
-    double alpha_double;
-    status = napi_get_value_double(env, args[3], &alpha_double);
-    alpha = (real) alpha_double;
+    napi_valuetype valuetype_alpha;
+    status = napi_typeof(env, args[3], &valuetype_alpha);
     assert(status == napi_ok);
-  }}
+    if (valuetype_alpha == napi_number) {
+      double alpha_double;
+      status = napi_get_value_double(env, args[3], &alpha_double);
+      alpha = (real) alpha_double;
+      assert(status == napi_ok);
+    }
+  }
 
   // window
   if (argc >= 5) {
-  napi_valuetype valuetype_window;
-  status = napi_typeof(env, args[4], &valuetype_window);
-  assert(status == napi_ok);
-  if (valuetype_window == napi_number) {
-    status = napi_get_value_int32(env, args[4], &window);
+    napi_valuetype valuetype_window;
+    status = napi_typeof(env, args[4], &valuetype_window);
     assert(status == napi_ok);
-  }}
+    if (valuetype_window == napi_number) {
+      status = napi_get_value_int32(env, args[4], &window);
+      assert(status == napi_ok);
+    }
+  }
 
   // min_count
   if (argc >= 6) {
-  napi_valuetype valuetype_min_count;
-  status = napi_typeof(env, args[5], &valuetype_min_count);
-  assert(status == napi_ok);
-  if (valuetype_min_count == napi_number) {
-    status = napi_get_value_int32(env, args[5], &min_count);
+    napi_valuetype valuetype_min_count;
+    status = napi_typeof(env, args[5], &valuetype_min_count);
     assert(status == napi_ok);
-  }}
+    if (valuetype_min_count == napi_number) {
+      status = napi_get_value_int32(env, args[5], &min_count);
+      assert(status == napi_ok);
+    }
+  }
 
   // sample
   if (argc >= 7) {
-  napi_valuetype valuetype_sample;
-  status = napi_typeof(env, args[6], &valuetype_sample);
-  assert(status == napi_ok);
-  if (valuetype_sample == napi_number) {
-    double sample_double;
-    status = napi_get_value_double(env, args[6], &sample_double);
-    sample = (real) sample_double;
+    napi_valuetype valuetype_sample;
+    status = napi_typeof(env, args[6], &valuetype_sample);
     assert(status == napi_ok);
-  }}
+    if (valuetype_sample == napi_number) {
+      double sample_double;
+      status = napi_get_value_double(env, args[6], &sample_double);
+      sample = (real) sample_double;
+      assert(status == napi_ok);
+    }
+  }
 
   // workers
   if (argc >= 8) {
-  napi_valuetype valuetype_workers;
-  status = napi_typeof(env, args[7], &valuetype_workers);
-  assert(status == napi_ok);
-  if (valuetype_workers == napi_number) {
-    status = napi_get_value_int32(env, args[7], &num_threads);
+    napi_valuetype valuetype_workers;
+    status = napi_typeof(env, args[7], &valuetype_workers);
     assert(status == napi_ok);
-  }}
+    if (valuetype_workers == napi_number) {
+      status = napi_get_value_int32(env, args[7], &num_threads);
+      assert(status == napi_ok);
+    }
+  }
 
   // hs
   if (argc >=9) {
-  napi_valuetype valuetype_hs;
-  status = napi_typeof(env, args[8], &valuetype_hs);
-  assert(status == napi_ok);
-  if (valuetype_hs == napi_number) {
-    status = napi_get_value_int32(env, args[8], &hs);
+    napi_valuetype valuetype_hs;
+    status = napi_typeof(env, args[8], &valuetype_hs);
     assert(status == napi_ok);
-  }}
+    if (valuetype_hs == napi_number) {
+      status = napi_get_value_int32(env, args[8], &hs);
+      assert(status == napi_ok);
+    }
+  }
 
   // negative
   if (argc >= 10) {
-  napi_valuetype valuetype_negative;
-  status = napi_typeof(env, args[9], &valuetype_negative);
-  assert(status == napi_ok);
-  if (valuetype_negative == napi_number) {
-    status = napi_get_value_int32(env, args[9], &negative);
+    napi_valuetype valuetype_negative;
+    status = napi_typeof(env, args[9], &valuetype_negative);
     assert(status == napi_ok);
-  }}
+    if (valuetype_negative == napi_number) {
+      status = napi_get_value_int32(env, args[9], &negative);
+      assert(status == napi_ok);
+    }
+  }
 
   // iter
   if (argc >= 11) {
-  napi_valuetype valuetype_iter;
-  status = napi_typeof(env, args[10], &valuetype_iter);
-  assert(status == napi_ok);
-  if (valuetype_iter == napi_number) {
-    double iter_double;
-    status = napi_get_value_double(env, args[10], &iter_double);
-    iter = (real) iter_double;
+    napi_valuetype valuetype_iter;
+    status = napi_typeof(env, args[10], &valuetype_iter);
     assert(status == napi_ok);
-  }}
+    if (valuetype_iter == napi_number) {
+      double iter_double;
+      status = napi_get_value_double(env, args[10], &iter_double);
+      iter = (real) iter_double;
+      assert(status == napi_ok);
+    }
+  }
 
   // cbow
   if (argc >= 3) {
-  napi_valuetype valuetype_cbow;
-  status = napi_typeof(env, args[11], &valuetype_cbow);
-  assert(status == napi_ok);
-  if (valuetype_cbow == napi_number) {
-    status = napi_get_value_int32(env, args[11], &cbow);
+    napi_valuetype valuetype_cbow;
+    status = napi_typeof(env, args[11], &valuetype_cbow);
     assert(status == napi_ok);
-  }}
+    if (valuetype_cbow == napi_number) {
+      status = napi_get_value_int32(env, args[11], &cbow);
+      assert(status == napi_ok);
+    }
+  }
   
   vocab = (struct vocab_word *)calloc(vocab_max_size, sizeof(struct vocab_word));
   vocab_hash = (int *)calloc(vocab_hash_size, sizeof(int));
